4fb6901ebc216445a565b73a564cf2964186f835
Chris Chen <christinafh.chen@gmail.com>
2018-06-06 20:48:48 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -31,6 +31,12 @@ end
 
 def deluxe_curfew_checker(time)
   # code goes here
+  if time == 11
+    "Time to apparate"
+  elsif time > 11
+  "You're in trouble! Better get home quick!"
+else time < 11 && time == 9
+  "Keep having fun!"
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
